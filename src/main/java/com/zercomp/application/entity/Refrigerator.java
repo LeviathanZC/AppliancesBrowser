a35502627d9fd6c61f4020c0d1cc43884502d502
LeviathanZC <leviathan.zercomp@gmail.com>
2020-08-24 12:30:55 +0300

    Describe ctr for Refrigerator

--- src/main/java/com/zercomp/application/entity/Refrigerator.java
+++ src/main/java/com/zercomp/application/entity/Refrigerator.java
@@ -2,6 +2,19 @@ package com.zercomp.application.entity;
 
 public class Refrigerator extends HouseholdAppliance {
 
-    
+    private int freezerCapacity;
+    private double overallCapacity;
+    private Size size;
+
+    public Refrigerator(int powerConsumption, int freezerCapacity, double overallCapacity, double width, double height) {
+        this(powerConsumption, freezerCapacity, overallCapacity, new Size(width, height));
+    }
+
+    public Refrigerator(int powerConsumption, int freezerCapacity, double overallCapacity, Size size) {
+        super(Refrigerator.class.getSimpleName(), powerConsumption);
+        this.freezerCapacity = freezerCapacity;
+        this.overallCapacity = overallCapacity;
+        this.size = size;
+    }
 
 }
