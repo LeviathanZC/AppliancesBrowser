8cd0d3e222999072d731711fc4ac96678af2e4dc
LeviathanZC <leviathan.zercomp@gmail.com>
2020-08-26 16:46:27 +0300

    Fix ApplianceDAO

--- src/main/java/com/zercomp/application/dal/ApplianceDAO.java
+++ src/main/java/com/zercomp/application/dal/ApplianceDAO.java
@@ -1,14 +1,15 @@
 package com.zercomp.application.dal;
 
+import com.zercomp.application.entity.AbstractAppliance;
 import com.zercomp.application.entity.criteria.Criteria;
 
 import java.util.List;
 
 public interface ApplianceDAO {
 
-    List<String> selectAll() throws DaoException;
+    List<AbstractAppliance> selectAll() throws DaoException;
 
-    //List<> findAppliance(Criteria criteria) throws DaoException;
+    List<AbstractAppliance> findAppliance(Criteria criteria) throws DaoException;
 
     //String se
 
