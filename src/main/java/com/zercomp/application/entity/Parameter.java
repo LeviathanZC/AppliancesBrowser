5bf90bd94620d3b305b10741a20a719e637cc497
LeviathanZC <leviathan.zercomp@gmail.com>
2020-09-01 16:59:51 +0300

    Create Parameter for parsing params from file.

---
+++ src/main/java/com/zercomp/application/entity/Parameter.java
@@ -0,0 +1,58 @@
+package com.zercomp.application.entity;
+
+public class Parameter {
+
+    private String name;
+    private String value;
+
+    public Parameter(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Parameter parameter = (Parameter) o;
+
+        if (getName() != null ? !getName().equals(parameter.getName()) : parameter.getName() != null) return false;
+        return getValue() != null ? getValue().equals(parameter.getValue()) : parameter.getValue() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Parameter{" +
+                "name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
